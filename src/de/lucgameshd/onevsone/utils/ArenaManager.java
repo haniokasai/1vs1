f73a3117ac6e64b863362767c5179a2fd86857a6
LucGamesYT <beckerluca16@gmail.com>
2016-07-28 11:18:15 +0200

    Arena Manager hinzugefügt

--- src/de/lucgameshd/onevsone/utils/ArenaManager.java
+++ src/de/lucgameshd/onevsone/utils/ArenaManager.java
@@ -1,5 +1,28 @@
 package de.lucgameshd.onevsone.utils;
 
+import java.io.File;
+import java.io.IOException;
+
+import cn.nukkit.utils.Config;
+
+
 public class ArenaManager {
 
+	private static File ordner = new File("plugins/1VS1");
+	public static File file = new File(ordner, "ArenaManager.yml");
+	public static Config cfg = new Config(file, 2);
+	
+	public static void createFiles(){
+		if(!ordner.exists()){
+			ordner.mkdirs();
+		}
+		if(!file.exists()){
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 }
