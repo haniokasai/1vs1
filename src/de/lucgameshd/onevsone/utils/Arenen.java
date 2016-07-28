4f1d29de51347f88e204242328693cd539fe139e
LucGamesYT <beckerluca16@gmail.com>
2016-07-28 13:26:58 +0200

    Update Arenen.

--- src/de/lucgameshd/onevsone/utils/Arenen.java
+++ src/de/lucgameshd/onevsone/utils/Arenen.java
@@ -3,6 +3,8 @@ package de.lucgameshd.onevsone.utils;
 import java.io.File;
 import java.util.List;
 
+import cn.nukkit.Player;
+import cn.nukkit.level.Position;
 import cn.nukkit.utils.Config;
 
 public class Arenen {
@@ -16,6 +18,22 @@ public class Arenen {
 		list.add(arena);
 		cfg.set("1vs1.Maps", list);
 		cfg.save();
+	}
+	
+	public static void setStart1(Player p, String arena){
+		LocationAPI.setArenaLocation("Start1", arena, p.getLocation());
+	}
+	
+	public static Position getStart1(Player p, String arenen){
+		return LocationAPI.getArenaLocation("Start1", arenen);
+	}
+	
+	public static void setStart2(Player p, String arena){
+		LocationAPI.setArenaLocation("Start2", arena, p.getLocation());
+	}
+	
+	public static Position getStart2(Player p, String arenen){
+		return LocationAPI.getArenaLocation("Start2", arenen);
 	}
 	
 	
