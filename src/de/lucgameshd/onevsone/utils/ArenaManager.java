0aa335894687e92a7ce2df389e62913eaebe0a0f
LucGamesYT <beckerluca16@gmail.com>
2016-07-28 11:26:20 +0200

    ArenaManager hinzugefügt.

--- src/de/lucgameshd/onevsone/utils/ArenaManager.java
+++ src/de/lucgameshd/onevsone/utils/ArenaManager.java
@@ -2,15 +2,19 @@ package de.lucgameshd.onevsone.utils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 import cn.nukkit.utils.Config;
-
 
 public class ArenaManager {
 
 	private static File ordner = new File("plugins/1VS1");
 	public static File file = new File(ordner, "ArenaManager.yml");
 	public static Config cfg = new Config(file, 2);
+	
+	public static ArrayList<String> isUsed = new ArrayList<String>();
 	
 	public static void createFiles(){
 		if(!ordner.exists()){
@@ -23,6 +27,54 @@ public class ArenaManager {
 				e.printStackTrace();
 			}
 		}
+	}
+	
+	public static void addInUsed(String arena){
+		if(!isUsed.contains(arena)){
+			isUsed.add(arena);
+		}
+	}
+	
+	public static void removeInUsed(String arena){
+		if(isUsed.contains(arena)){
+			isUsed.remove(arena);
+		}
+	}
+	
+	public static boolean isUsed(String arena){
+		if(isUsed.contains(arena)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public static String searchForArena(){
+		List<String> list = cfg.getStringList("1vs1.Maps");
+		Random r = new Random();
+		boolean found = false;
+		while(found == false){
+			int rn = r.nextInt(list.size());
+			if(isUsed(list.get(rn))){
+				continue;
+			}else{
+				found = true;
+				return searchInList();
+			}
+		}
+		return "NothingFound!";
+	}
+	
+	public static String searchInList(){
+		List<String> list = cfg.getStringList("1vs1.Maps");
+		for(String s : list){
+			if(isUsed(s)){
+				continue;
+			}else{
+				return s;
+			}
+		}
+		return "NothingFound!";
 	}
 	
 }
