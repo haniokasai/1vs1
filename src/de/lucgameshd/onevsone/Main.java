95fc08a495c076a7f05b2349afef35c70533ef44
LucGamesYT <beckerluca16@gmail.com>
2016-07-28 13:16:20 +0200

    Main hinzugefügt.

--- src/de/lucgameshd/onevsone/Main.java
+++ src/de/lucgameshd/onevsone/Main.java
@@ -1,5 +1,34 @@
 package de.lucgameshd.onevsone;
 
-public class Main {
+import cn.nukkit.plugin.PluginBase;
+import de.lucgameshd.onevsone.utils.ArenaManager;
+import de.lucgameshd.onevsone.utils.LocationAPI;
+
+public class Main extends PluginBase{
+	
+	
+	@Override
+	public void onEnable() {
+		this.registerCommands();
+		this.registerListeners();
+		ArenaManager.createFiles();
+		LocationAPI.createFiles();
+		super.onEnable();
+	}
+	
+	@Override
+	public void onDisable() {
+
+		super.onDisable();
+	}
+	
+	
+	private void registerCommands(){
+		
+	}
+	
+	private void registerListeners(){
+		
+	}
 
 }
