565d2010ee7b59fa2a9ba5c39037f24a7996386b
LucGamesYT <beckerluca16@gmail.com>
2016-07-29 15:13:05 +0200

    Paar neue Sachen hinzugefügt.

--- src/de/lucgameshd/onevsone/utils/Arenen.java
+++ src/de/lucgameshd/onevsone/utils/Arenen.java
@@ -16,33 +16,61 @@ public class Arenen {
 	
 	
 	public static void createArena(String arena){
-		List<String> list = cfg.getStringList("1vs1.Arenan");
+		List<String> list = cfg.getStringList("1vs1.Arenen");
 		list.add(arena);
-		cfg.set("1vs1.Arenan", list);
-		cfg.set("1vs1.Arenan." + arena + ".Players", new ArrayList<>());
+		cfg.set("1vs1.Arenen.Names", list);
+		cfg.set("1vs1.Arenen." + arena + ".Status", "Frei");
+		cfg.set("1vs1.Arenen." + arena + ".Players", new ArrayList<>());
 		cfg.save();
+	}
+	
+	public static void resetArenen(){
+		for(String arena : cfg.getStringList("1vs1.Arenen")){
+			cfg.set("1vs1.Arenen." + arena + ".Status", "Frei");
+			cfg.set("1vs1.Arenen." + arena + ".Players", new ArrayList<>());
+			cfg.save();
+		}
 	}
 	
 	public static void addPlayer(String arena, String player){
 		List<String> players = getPlayers(arena);
 		players.add(player);
-		cfg.set("1vs1.Arenan." + arena + ".Players", players);
+		cfg.set("1vs1.Arenen." + arena + ".Players", players);
 		cfg.save();
 	}
 	
 	public static void removePlayer(String arena, String player){
 		List<String> players = getPlayers(arena);
 		players.remove(player);
-		cfg.set("1vs1.Arenan." + arena + ".Players", players);
+		cfg.set("1vs1.Arenen." + arena + ".Players", players);
 		cfg.save();
 	}
 	
 	public static List<String> getPlayers(String arena) {
-		return cfg.getStringList("1vs1.Arenan." + arena + ".Players");
+		return cfg.getStringList("1vs1.Arenen." + arena + ".Players");
+	}
+	
+	public static void setStatus(String arena, String status){
+		cfg.set("1vs1.Arenen." + arena + ".Status", status);
+		cfg.save();
+	}
+	
+	public static String getStatus(String arena){
+		return cfg.getString("1vs1.Arenen." + arena + ".Status");
+	}
+	
+	public static String getArena(Player p){
+		for(String arena : cfg.getStringList("1vs1.Arenen.Names")){
+			List<String> players = getPlayers(arena);
+			if(players.contains(p.getName())){
+				return arena;
+			}
+		}
+		return "ArenaNotFound!";
 	}
 	
 	public static boolean isInArena(Player p){
-		for(String arena : cfg.getStringList("1vs1.Arenan")){
+		for(String arena : cfg.getStringList("1vs1.Arenen.Names")){
 			List<String> players = getPlayers(arena);
 			if(players.contains(p.getName())){
 				return true;
